b2a8ac8da4f04ef1c3d1c66fbebe0ce61baef028
dflemming <daisy.flemming@kaleido.com>
2019-08-28 22:27:19 -0400

    save all I have

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 Application that processes the platemaps as well as tracks sequencing plates and open wells
 
-# 
+# Environment variables
+* During development, please add a .env file that contains the following properties:
+
+  KAPTURE_JWT_TOKEN=replace_with_a_valid_token
+
+* During deployment, create the lambda function with the --environment attribute
+  ```
+  aws lambda create-function \
+      --region us-east-1 \
+      --function-name publishPlateMapSetUp \
+      --zip-file fileb://publishPlateMapSetUp.zip \
+      --role arn:aws:iam::001507046168:role/service-role/DynamoDBKickingLambdaRole \
+      --handler publishPlateMapSetUp.handler \
+      --timeout 5 \
+      --runtime nodejs10.x
+      --environment Variables="{KAPTURE_JWT_TOKEN=replace_with_a_valid_token}" 
+  ```
--- publishPlateMapSetUp.js
+++ publishPlateMapSetUp.js
@@ -1,8 +1,12 @@
 'use strict';
+require('dotenv').config();
 var AWS = require("aws-sdk");
 var sns = new AWS.SNS();
 var axios = require("axios");
-var url = 'http://kapture-staging.apps.kaleidobio.com/api/platemaps/atlas';
+// var url = 'https://kapture-staging.apps.kaleidobio.com/api/platemaps/atlas';
+var url = 'http://localhost:8080/api/platemaps/atlas';
+
+const token = process.env.KAPTURE_JWT_TOKEN;
 
 exports.handler = (event, context, callback) => {
 
@@ -11,8 +15,8 @@ exports.handler = (event, context, callback) => {
         console.log('eventType', record.eventName);
         var image = record.dynamodb.NewImage?record.dynamodb.NewImage:record.dynamodb.OldImage;
 
-        var experiment = image.experiment && image.experiment.S?JSON.stringify(image.experiment.S):'';
-        var status = image.experiment && image.experiment.S?JSON.stringify(image.status.S):'';
+        var experiment = image.experiment && image.experiment.S?image.experiment.S:null;
+        var status = image.experiment && image.experiment.S?image.status.S:null;
         var plateMaps = image.plateMaps && image.plateMaps.S?JSON.parse(image.plateMaps.S):null;
         if (status === 'COMPLETE') {
             saveToKapture(experiment, plateMaps);
@@ -35,9 +39,16 @@ exports.handler = (event, context, callback) => {
     callback(null, `Successfully processed ${event.Records.length} records.`);
 };
 
-function saveToKapture(experiment, plateMaps){
-    var plateMapsToSave = formatPlateMap(experiment, plateMaps);
-    axios.post(url, plateMapsToSave)
+function saveToKapture(experiment, plateMaps) {
+    var wellsToSave = formatWells(experiment, plateMaps);
+    axios.post(url,
+        {
+            experimentName: experiment,
+            wells: wellsToSave
+        }, {
+            headers: {"Authorization": `Bearer ${token}`}
+        }
+    )
         .then(function (response) {
             console.log(response);
         })
@@ -46,23 +57,21 @@ function saveToKapture(experiment, plateMaps){
         });
 }
 
-function formatPlateMap(experiment, plateMaps) {
-    var plateMapsToSave = [];
+function formatWells(experiment, plateMaps) {
+    var wellsToSave = [];
     plateMaps.forEach(function(plateMap){
         var thePlateMap = { id:null, plateNumber: plateMap.id, platePurpose:null, plateType:null };
         plateMap.data.forEach(function(row){
-            var wells = [];
             var re0 = /[0-9]/g;
             var re1 = /[a-zA-Z]/g;
             row.forEach(function (cell){
-                var rowLabel = cell.id.replace(re1,'');
-                var colLabel= cell.id.replace(re0,'');
+                var rowLabel = cell.id.replace(re0,'');
+                var colLabel= cell.id.replace(re1,'');
                 var theSample = { id:null, label: experiment+".p"+plateMap.id+"."+cell.id };
                 var theWell = { id: null, platemap:thePlateMap, sample:theSample, row: rowLabel, column: colLabel };
-                wells.push( theWell );
+                wellsToSave.push( theWell );
             });
-            plateMapsToSave.push({Platemap: thePlateMap, Wells: wells})
         });
     });
-    return plateMapsToSave;
+    return wellsToSave;
 }
